124875496ffa8e4e128e182ef15cfe063fd735ae
MangKuns <kusumarizky65@gmail.com>
2024-03-04 07:58:38 +0700

    last commit

--- Jurnal_MOD_[3]_1302220013_Rizky Kusuma Nugraha/Form1.Designer.cs
+++ Jurnal_MOD_[3]_1302220013_Rizky Kusuma Nugraha/Form1.Designer.cs
@@ -28,12 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tombol1 = new Button();
+            tombol2 = new Button();
+            tombol3 = new Button();
+            tombol4 = new Button();
+            tombol5 = new Button();
+            tombol6 = new Button();
+            tombol7 = new Button();
+            tombol8 = new Button();
+            tombol9 = new Button();
+            tombol0 = new Button();
+            tombolSD = new Button();
+            tombolPlus = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // tombol1
+            // 
+            tombol1.Location = new Point(102, 70);
+            tombol1.Name = "tombol1";
+            tombol1.Size = new Size(33, 33);
+            tombol1.TabIndex = 0;
+            tombol1.Text = "1";
+            tombol1.UseVisualStyleBackColor = true;
+            tombol1.Click += tombol1_Click;
+            // 
+            // tombol2
+            // 
+            tombol2.Location = new Point(141, 70);
+            tombol2.Name = "tombol2";
+            tombol2.Size = new Size(33, 33);
+            tombol2.TabIndex = 16;
+            tombol2.Text = "2";
+            tombol2.UseVisualStyleBackColor = true;
+            tombol2.Click += tombol2_Click;
+            // 
+            // tombol3
+            // 
+            tombol3.Location = new Point(180, 70);
+            tombol3.Name = "tombol3";
+            tombol3.Size = new Size(33, 33);
+            tombol3.TabIndex = 17;
+            tombol3.Text = "3";
+            tombol3.UseVisualStyleBackColor = true;
+            tombol3.Click += tombol3_Click;
+            // 
+            // tombol4
+            // 
+            tombol4.Location = new Point(102, 109);
+            tombol4.Name = "tombol4";
+            tombol4.Size = new Size(33, 33);
+            tombol4.TabIndex = 18;
+            tombol4.Text = "4";
+            tombol4.UseVisualStyleBackColor = true;
+            tombol4.Click += tombol4_Click;
+            // 
+            // tombol5
+            // 
+            tombol5.Location = new Point(141, 109);
+            tombol5.Name = "tombol5";
+            tombol5.Size = new Size(33, 33);
+            tombol5.TabIndex = 19;
+            tombol5.Text = "5";
+            tombol5.UseVisualStyleBackColor = true;
+            tombol5.Click += tombol5_Click;
+            // 
+            // tombol6
+            // 
+            tombol6.Location = new Point(180, 109);
+            tombol6.Name = "tombol6";
+            tombol6.Size = new Size(33, 33);
+            tombol6.TabIndex = 20;
+            tombol6.Text = "6";
+            tombol6.UseVisualStyleBackColor = true;
+            tombol6.Click += tombol6_Click;
+            // 
+            // tombol7
+            // 
+            tombol7.Location = new Point(102, 148);
+            tombol7.Name = "tombol7";
+            tombol7.Size = new Size(33, 33);
+            tombol7.TabIndex = 21;
+            tombol7.Text = "7";
+            tombol7.UseVisualStyleBackColor = true;
+            tombol7.Click += tombol7_Click;
+            // 
+            // tombol8
+            // 
+            tombol8.Location = new Point(141, 148);
+            tombol8.Name = "tombol8";
+            tombol8.Size = new Size(33, 33);
+            tombol8.TabIndex = 22;
+            tombol8.Text = "8";
+            tombol8.UseVisualStyleBackColor = true;
+            tombol8.Click += tombol8_Click;
+            // 
+            // tombol9
+            // 
+            tombol9.Location = new Point(180, 148);
+            tombol9.Name = "tombol9";
+            tombol9.Size = new Size(33, 33);
+            tombol9.TabIndex = 23;
+            tombol9.Text = "9";
+            tombol9.UseVisualStyleBackColor = true;
+            tombol9.Click += tombol9_Click;
+            // 
+            // tombol0
+            // 
+            tombol0.Location = new Point(141, 187);
+            tombol0.Name = "tombol0";
+            tombol0.Size = new Size(33, 33);
+            tombol0.TabIndex = 24;
+            tombol0.Text = "0";
+            tombol0.UseVisualStyleBackColor = true;
+            tombol0.Click += tombol0_Click;
+            // 
+            // tombolSD
+            // 
+            tombolSD.Location = new Point(268, 109);
+            tombolSD.Name = "tombolSD";
+            tombolSD.Size = new Size(33, 33);
+            tombolSD.TabIndex = 25;
+            tombolSD.Text = "=";
+            tombolSD.UseVisualStyleBackColor = true;
+            tombolSD.Click += tombolSD_Click;
+            // 
+            // tombolPlus
+            // 
+            tombolPlus.Location = new Point(268, 70);
+            tombolPlus.Name = "tombolPlus";
+            tombolPlus.Size = new Size(33, 33);
+            tombolPlus.TabIndex = 26;
+            tombolPlus.Text = "+";
+            tombolPlus.UseVisualStyleBackColor = true;
+            tombolPlus.Click += tombolPlus_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(103, 45);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(110, 23);
+            textBox1.TabIndex = 27;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(tombolPlus);
+            Controls.Add(tombolSD);
+            Controls.Add(tombol0);
+            Controls.Add(tombol9);
+            Controls.Add(tombol8);
+            Controls.Add(tombol7);
+            Controls.Add(tombol6);
+            Controls.Add(tombol5);
+            Controls.Add(tombol4);
+            Controls.Add(tombol3);
+            Controls.Add(tombol2);
+            Controls.Add(tombol1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button tombol1;
+        private Button tombol2;
+        private Button tombol3;
+        private Button tombol4;
+        private Button tombol5;
+        private Button tombol6;
+        private Button tombol7;
+        private Button tombol8;
+        private Button tombol9;
+        private Button tombol0;
+        private Button tombolSD;
+        private Button tombolPlus;
+        private TextBox textBox1;
     }
 }
--- Jurnal_MOD_[3]_1302220013_Rizky Kusuma Nugraha/Form1.cs
+++ Jurnal_MOD_[3]_1302220013_Rizky Kusuma Nugraha/Form1.cs
@@ -2,9 +2,96 @@ namespace Jurnal_MOD__3__1302220013_Rizky_Kusuma_Nugraha
 {
     public partial class Form1 : Form
     {
+        static int temp = 0;
+        static string output = "";
+        static int hasil = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tombol1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol1.Text;
+            output = output + tombol1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tombolPlus_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombolPlus.Text;
+            temp = int.Parse(output);
+            hasil = hasil + temp;
+            temp = 0;
+            output = "";
+        }
+
+        private void tombolSD_Click(object sender, EventArgs e)
+        {
+            temp = int.Parse(output);
+            hasil = hasil + temp;
+            string keluar = Convert.ToString(hasil);
+            textBox1.Text = keluar;
+            temp = 0;
+            output = "";
+        }
+
+        private void tombol2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol2.Text;
+            output = output + tombol2.Text;
+        }
+
+        private void tombol3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol3.Text;
+            output = output + tombol3.Text;
+        }
+
+        private void tombol4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol4.Text;
+            output = output + tombol4.Text;
+        }
+
+        private void tombol5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol5.Text;
+            output = output + tombol5.Text;
+        }
+
+        private void tombol6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol6.Text;
+            output = output + tombol6.Text;
+        }
+
+        private void tombol7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol7.Text;
+            output = output + tombol7.Text;
+        }
+
+        private void tombol8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol8.Text;
+            output = output + tombol8.Text;
+        }
+
+        private void tombol9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol9.Text;
+            output = output + tombol9.Text;
+        }
+
+        private void tombol0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + tombol0.Text;
+            output = output + tombol0.Text;
         }
     }
 }
