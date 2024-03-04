49c1617ae1b1aa875735ad148e80e5da16332aaa
MangKuns <kusumarizky65@gmail.com>
2024-03-04 07:02:23 +0700

    first commit

---
+++ Jurnal_MOD_[3]_1302220013_Rizky Kusuma Nugraha/Form1.cs
@@ -0,0 +1,10 @@
+namespace Jurnal_MOD__3__1302220013_Rizky_Kusuma_Nugraha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Jurnal_MOD_[3]_1302220013_Rizky Kusuma Nugraha/Program.cs
@@ -0,0 +1,17 @@
+namespace Jurnal_MOD__3__1302220013_Rizky_Kusuma_Nugraha
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
